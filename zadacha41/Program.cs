cb86660e035767d4ae6f892fa69155431e81bf72
thenPie <118397975+thenPie@users.noreply.github.com>
2022-12-17 01:47:26 +0300

    Initial commit, did everything

---
+++ zadacha41/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пять чисел через пробел: ");
+
+var input = Console.ReadLine();
+var space = input.Split(" ");
+int num1 = int.Parse(space[0]);
+int num2 = int.Parse(space[1]);
+int num3 = int.Parse(space[2]);
+int num4 = int.Parse(space[3]);
+int num5 = int.Parse(space[4]);
+
+int[] nums = {num1, num2, num3, num4, num5};
+int plus = 0;
+
+for(int i = 0; i < nums.Length; i++) {
+    if(0 < nums[i]) {
+        plus++;
+    }
+}
+
+Console.Write(plus);
---
+++ zadacha43/Program.cs
@@ -0,0 +1,14 @@
+﻿// double k1 = 5, b1 = 2, k2 = 9, b2 = 4;
+
+Console.WriteLine("k1, b1 - первая, k2, b2 - вторая, в такой очереди через пробел");
+var input = Console.ReadLine();
+var space = input.Split(" ");
+double k1 = int.Parse(space[0]);
+double b1 = int.Parse(space[1]);
+double k2 = int.Parse(space[2]);
+double b2 = int.Parse(space[3]);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($"({x}; {y})");
